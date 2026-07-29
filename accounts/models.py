4792a6bcae5d2a764aba14be47311704c6b3c860
waniya farooq <waniyafarooq18@gmail.com>
2026-07-30 01:59:49 +0500

    Add custom User model and set AUTH_USER_MODEL in settings

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('STUDENT','Student'),
+        ('TEACHER','Teacher'),
+        ('ADMIN','Admin')
+    )
+    role = models.CharField(
+        max_length=20,choices=ROLE_CHOICES,default='STUDENT'
+    )
